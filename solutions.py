94d168121f2b34bb08796f31fdf55c77d6b9c4c1
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-09-19 17:45:55 +0500

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -104,3 +104,17 @@ def quicksort(arr):
         return quicksort(left) + middle + quicksort(right)
         
 print(quicksort([3, 2, 1]))
+
+
+# quick sort for descending
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x > pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x < pivot]
+        return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([1, 56, 3]))
